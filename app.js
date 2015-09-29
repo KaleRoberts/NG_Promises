f265aaa4ea567122f234627cd968858768f53c7a
kroberts <kroberts@netspend.com>
2015-09-29 14:22:48 -0500

    Demonstrating promise chaining

--- app.js
+++ app.js
@@ -19,17 +19,14 @@
 	
 	app.run(function(getData) {
 		var promise = getData()
-		.then(function(string) {
-			console.log(string);
-		}, function (error) {		// This is the second parameter of the then() method. It's an optional error handling callback function that is called iff the promise is rejected.
-			console.error(error);
-		})
-		.then(function() {	// Mutliple callback functions can be registered to the same promise object by amking different calls to the then() method.
-			console.log("This is another new promise resolution of the getData function");	// Functions are executed in the order that they are registered. 
-		})	// So  regardless of if the first promise is resolved or not we will get this second one saying, "This is another promise resolution"
-		.finally(function() {	// The finally() method is executed regardless of whether or not the success or error callbacks were invoked. 
-			console.log('Finished at: ', new Date());	// This can be used to reset a form or something to a pristine state. (Angular has a pristine state for forms I think actually)
-		})
+			.then(function(num) {
+				console.log(num);
+				return num * 2;		// This is deomonstrating promise chaining. I'm return something from the first promise callback and then using in a subsequent callback.
+			})						// Functions can be returned from promise callbacks, you could return a new promise as well.
+									// The promise chain "pauses" until the returned promise resolves, allowing chaining of multiple async function calls.
+			.then(function(num) {
+				console.log(num); // Which will be the random number from getData and the first promise resolution multiplied by 2
+			})
 	});
 
 }());
